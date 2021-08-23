a375cb4f05827d4f74039511effa8c7c6fb5fa2e
Tendai <tendai1@hotmail.com>
2021-08-23 12:25:45 +0200

    refactor code in the index js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Todocontainer from './components/Todocontainer'
 
-const element = <h1>Hello From Create React App</h1>
 
 
-ReactDOM.render(element, document.getElementById('root')
+
+ReactDOM.render(<Todocontainer />, document.getElementById('root')
 );
