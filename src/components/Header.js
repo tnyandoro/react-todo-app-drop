db6cc9e1a39d2d4f7078c4497fd3fd23e7a9d502
Tendai <tendai1@hotmail.com>
2021-08-23 12:21:06 +0200

    Add the class compnent for the todo container

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React React App</h1>
+        <p>i am a react Component</p>
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
